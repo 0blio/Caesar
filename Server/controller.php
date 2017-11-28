ca96cf72a8d1a50db1663e43e9af53bd4c61c772
Michele Cisternino <33893182+0blio@users.noreply.github.com>
2017-11-28 09:49:53 +0100

    Update controller.php

--- Server/controller.php
+++ Server/controller.php
@@ -13,29 +13,39 @@
 	session_start();
 
 	// If user is logged in
-	if (isset($_SESSION['username'])) {
-		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	if (isset($_SESSION['username']) and isset($_SESSION['csrf_token'])) {
+		if ($_SERVER['REQUEST_METHOD'] == 'GET' and isset($_GET['csrf_token'])) {
+			
+			// If CSRF token is valid
+			if ($_GET['csrf_token'] == $_SESSION['csrf_token']) {
 
-			include 'database/connection.php';
+				include 'database/connection.php';
 
-			include 'helpers/server/string.php';
-			include 'helpers/server/html.php';
+				include 'helpers/server/string.php';
+				include 'helpers/server/html.php';
 
-			// If no target is selected 
-			if (isset($_GET['command']) and !isset($_GET['target'])) 
-				include 'controller-logic/non_selected_target.php';
+				// If no target is selected 
+				if (isset($_GET['command']) and !isset($_GET['target'])) 
+					include 'controller-logic/non_selected_target.php';
 		
-			// Else if a target is selected
-			else if (isset($_GET['command']) and isset($_GET['target']) and !isset($_GET['shell'])) 
-				include 'controller-logic/selected_target.php';
+				// Else if a target is selected
+				else if (isset($_GET['command']) and isset($_GET['target']) and !isset($_GET['shell'])) 
+					include 'controller-logic/selected_target.php';
 
-			// Else if the user is using a pseudo-interactive shell
-			else if (isset($_GET['command']) and isset($_GET['target']) and isset($_GET['shell'])) 
-				include 'controller-logic/target_shell.php';
+				// Else if the user is using a pseudo-interactive shell
+				else if (isset($_GET['command']) and isset($_GET['target']) and isset($_GET['shell'])) 
+					include 'controller-logic/target_shell.php';
 
-			else 
-				$output = 'No arguments';
+				else 
+					$output = 'No arguments';
 
-			echo $output;
+				echo $output;
+			} else {
+				echo 'Invalid CSRF token!';
+			}
+		} else {
+			echo 'Invalid request!';
 		}		
-	} 
+	} else {
+		echo 'You are not logged in!';
+	}
